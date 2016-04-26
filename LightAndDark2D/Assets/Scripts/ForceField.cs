f98ae2074abed5cce7057f8adc2cf58b73e0004e
Oribow <Oribow@users.noreply.github.com>
2016-04-26 19:09:50 +0200

    implemented basic CharacterController
    First draft of a CharacterController.
    Bugs:
    - sometimes jumps dont trigger
    Todo:
    - analog jumping
    - rocket jumping
    - other movement

---
+++ LightAndDark2D/Assets/Scripts/ForceField.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForceField : MonoBehaviour {
+
+    public Vector2 force;
+    public bool linearLess;
+    public Vector2 anker;
+    public float length;
+	
+	void OnTriggerEnter2D (Collider2D other) {
+        ICharacterControllerInput2D iInput = other.GetComponent<ICharacterControllerInput2D>();
+        if (iInput != null)
+        {
+            iInput.AddConstantForce(force);
+        }
+	}
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        ICharacterControllerInput2D iInput = other.GetComponent<ICharacterControllerInput2D>();
+        if (iInput != null)
+        {
+            iInput.RemoveConstantForce(force);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        if (linearLess)
+        {
+            Gizmos.DrawRay(anker, force * length);
+        }
+    }
+}
