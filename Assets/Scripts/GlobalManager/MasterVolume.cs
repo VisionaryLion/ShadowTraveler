eb59d81dcdd669f8167e32db32594b7f7f40e744
Oribow <spok1000@web.de>
2016-09-07 18:15:34 +0200

    Implemented Footsteps & Master Volume

---
+++ Assets/Scripts/GlobalManager/MasterVolume.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MasterVolume : MonoBehaviour {
+
+    [Range(0, 100)]
+    public float volume = 1.0f;
+    FMOD.Studio.Bus masterBus;
+
+    void Start()
+    {
+        masterBus = FMODUnity.RuntimeManager.GetBus("bus:/");
+        
+    }
+
+    void Update()
+    {
+        masterBus.setFaderLevel(volume);
+    }
+}
