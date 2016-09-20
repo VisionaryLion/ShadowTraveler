c393bf1c239ef26f2752a61fb66ca8ff02881c0e
ryandicenzo <dicenzoryan@gmail.com>
2016-09-19 19:19:47 -0700

    Base UI for Pause/Death

---
+++ Assets/ButtonHandlers.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+namespace Manager
+{
+    public class ButtonHandlers : MonoBehaviour
+    {
+
+        public void Resume()
+        {
+            GameStateManager.GetInstance().EndCurrentState();
+        }
+
+        public void Menu()
+        {
+            SceneManager.LoadScene("Main Menu");
+        }
+
+        public void Quit()
+        {
+            Application.Quit();
+        }
+
+        public void Restart()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
