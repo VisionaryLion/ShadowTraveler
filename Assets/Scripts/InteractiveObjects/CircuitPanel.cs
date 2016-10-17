5432620b88c94f956644a28be9cf8d92e4742b73
ryandicenzo <dicenzoryan@gmail.com>
2016-10-16 20:51:24 -0700

    Changed Sprite import settings and Added Circuit Functionality

---
+++ Assets/Scripts/InteractiveObjects/CircuitPanel.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using Actors;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class CircuitPanel : MonoBehaviour {
+
+    PlayerActor player;
+    [SerializeField]
+    GameObject enable;
+    [SerializeField]
+    GameObject disable;
+
+    void Awake()
+    {
+        player = ActorDatabase.GetInstance().FindFirst<PlayerActor>();
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if(coll.CompareTag("Player"))
+        {
+            if (player.EquipmentManager.equipmentSpawnPointLeft.FindChild("Circuit") != null)
+            {
+                enable.SetActive(true);
+                disable.SetActive(false);
+                GameObject circuit = player.EquipmentManager.equipmentSpawnPointLeft.FindChild("Circuit").gameObject;
+
+                circuit.transform.SetParent(transform);
+                circuit.transform.rotation = Quaternion.identity;
+
+                Vector3 position = new Vector3(0.005f, 0.135f, 0);
+                circuit.transform.localPosition = position;
+
+
+            }
+        }
+    }	
+}
