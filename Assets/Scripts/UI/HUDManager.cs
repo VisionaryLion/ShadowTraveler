34235c7f7f1adfb5d5a8af42554564db0fa3cf69
ryandicenzo <dicenzoryan@gmail.com>
2016-12-12 18:55:09 -0800

    Added Durability Bar

--- Assets/Scripts/UI/HUDManager.cs
+++ Assets/Scripts/UI/HUDManager.cs
@@ -15,6 +15,8 @@ namespace ItemHandler
         Text leftItemDescription;
         [SerializeField]
         Image leftItemIcon;
+        [SerializeField]
+        Image leftItemDurability;
 
         [Header("Right HUD")]
         [SerializeField]
@@ -23,10 +25,18 @@ namespace ItemHandler
         Text rightItemDescription;
         [SerializeField]
         Image rightItemIcon;
+        [SerializeField]
+        Image rightItemDurability;
 
         PlayerActor player;
         Sprite emptySprite = null;
         public static HUDManager hudManager;
+
+        public bool decreaseLeft = false;
+        public bool decreaseRight = false;
+
+        DurableItem leftDurableItem;
+        DurableItem rightDurableItem;
 
         void Awake()
         {
@@ -44,6 +54,7 @@ namespace ItemHandler
             leftItemName.text = item.Title;
             leftItemIcon.enabled = true;
             leftItemIcon.sprite = item.Icon;
+            leftItemDurability.fillAmount = 1f;
         }
 
         public void EquipRight(IItem item)
@@ -52,6 +63,7 @@ namespace ItemHandler
             rightItemName.text = item.Title;
             leftItemIcon.enabled = true;
             rightItemIcon.sprite = item.Icon;
+            rightItemDurability.fillAmount = 1f;
         }
 
         public void EmptyLeft()
@@ -59,6 +71,7 @@ namespace ItemHandler
             leftItemDescription.text = "";
             leftItemName.text = "";
             leftItemIcon.enabled = false;
+            leftItemDurability.fillAmount = 0;
         }
 
         public void EmptyRight()
@@ -66,11 +79,34 @@ namespace ItemHandler
             rightItemDescription.text = "";
             rightItemName.text = "";
             rightItemIcon.enabled = false;
+            rightItemDurability.fillAmount = 0;
+        }
+
+        public void startBurn(bool right, DurableItem item)
+        {
+            if (right)
+            {
+                decreaseRight = true;
+                rightDurableItem = item;
+            }
+            else
+            {
+                decreaseLeft = true;
+                leftDurableItem = item;
+            }
+        }
+
+        void Update()
+        {
+            if (decreaseLeft)
+            {
+                leftItemDurability.fillAmount = leftDurableItem.duration / 100;
+            }
+
+            if (decreaseRight)
+            {
+                rightItemDurability.fillAmount = rightDurableItem.duration / 100;
+            }
         }
     }
 }
-
-
-
-
-
