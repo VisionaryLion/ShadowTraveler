86440f0d4cf9b449d3dcf9aafb442248c5a4ec7f
VisionaryLion <kylebrennan@radiogenicgames.com>
2017-10-08 17:25:20 +0200

    Imported trap assets and started tutorial character scripting

---
+++ Assets/Scenes/Kyle/Tutorials/Casanis/2D Prototyping in Unity/Scripts/playerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+    //movement variables
+    public float maxSpeed;
+
+    Rigidbody2D myRB; //reference to the rigidbody on the player
+    Animator myAnim; //reference to the player's animator
+    
+    // Use this for initialization
+	void Start () {
+        myRB = GetComponent<Rigidbody2D>(); //GetComponent looks at the asset the script is attached to for a certain object
+        myAnim = GetComponent<Animator>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
